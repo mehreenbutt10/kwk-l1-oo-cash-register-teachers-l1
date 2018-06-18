5508788390019701a578940f62fb22ddf6ee5361
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-18 05:14:00 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,22 @@
-# Code your cash register here!
+class AmazonTransaction
+  
+  
+  def initialize(discount = 20)
+    @total = 0 
+    @items = []
+    @discount = discount
+  end
+  
+  def total
+    @total
+  end
+  
+  def total=(total)
+    @total=total
+  end
+  
+  def add_item(title, price)
+   
+    @total
+  end
+end
